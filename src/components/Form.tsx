376869b47fc962dd3cd87d489e883545f999bb8c
eobcre <eobcre@gmail.com>
2023-08-25 23:58:10 -0400

    created form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,14 @@
 const Form = () => {
   return (
     <div>
-      <p className='form-title'>Or Sign Up With Email</p>
+      <form>
+        <p className='form-title'>Or Sign Up With Email</p>
+        <label htmlFor='name'>Full Name</label>
+        <input type='text' id='name' placeholder='Eg. Mary Olhson' />
+
+        <label htmlFor='name'>Email Address</label>
+        <input type='text' id='name' placeholder='your@email.com' />
+      </form>
     </div>
   );
 };
