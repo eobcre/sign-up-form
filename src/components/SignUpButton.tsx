f499f20f8fc34a3f635349d0c1e0917955f33ede
eobcre <eobcre@gmail.com>
2023-08-26 20:44:54 -0400

    custom font-family removed

--- src/components/SignUpButton.tsx
+++ src/components/SignUpButton.tsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 const SignUpButton: React.FC = () => {
   return (
     <div>
-      <button className='flex items-center gap-8 bg-[#5B87E8] text-white font-arial rounded pl-1 pr-7 py-2'>
+      <button className='flex items-center gap-8 bg-[#5B87E8] text-white rounded pl-1 pr-7 py-2'>
         <Icon
           icon='flat-color-icons:google'
           className='bg-white rounded p-2 w-[38px] h-[38px]'
