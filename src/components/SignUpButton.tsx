21756feda914d204dbd949f2e9d0e6d4775588bc
eobcre <eobcre@gmail.com>
2023-08-26 22:09:20 -0400

    updated padding

--- src/components/SignUpButton.tsx
+++ src/components/SignUpButton.tsx
@@ -2,8 +2,8 @@ import { Icon } from '@iconify/react';
 
 const SignUpButton: React.FC = () => {
   return (
-    <div>
-      <button className='flex items-center gap-8 bg-[#5B87E8] text-white rounded pl-1.5 pr-7 py-1.5'>
+    <div className='flex justify-center'>
+      <button className='flex items-center gap-8 bg-[#5B87E8] text-white rounded pl-1.5 py-1.5 w-full'>
         <Icon
           icon='flat-color-icons:google'
           className='bg-white rounded p-1 w-[30px] h-[30px]'
