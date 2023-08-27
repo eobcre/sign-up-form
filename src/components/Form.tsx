3da303d608afe9a8b8b75d068b06f4aa6586a598
eobcre <eobcre@gmail.com>
2023-08-26 22:08:57 -0400

    form styled

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,32 @@
 const Form: React.FC = () => {
   return (
-    <div>
+    <div className='py-8'>
       <form>
-        <p className='border-line flex justify-center items-center text-[12px] text-[#B4B4B4] uppercase py-8'>
+        <p className='border-line flex justify-center items-center text-[12px] text-[#B4B4B4] uppercase pb-3'>
           Or Sign Up With Email
         </p>
-        <label htmlFor='name'>Full Name</label>
-        <input type='text' id='name' placeholder='Eg. Mary Olhson' />
-
-        <label htmlFor='name'>Email Address</label>
-        <input type='text' id='name' placeholder='your@email.com' />
+        <div>
+          <label htmlFor='name' className='text-sm'>
+            Full Name
+          </label>
+          <input
+            type='text'
+            id='name'
+            placeholder='Eg. Mary Olhson'
+            className='text-sm border rounded mb-3 px-4 py-2 w-[225px]'
+          />
+        </div>
+        <div>
+          <label htmlFor='name' className='text-sm'>
+            Email Address
+          </label>
+          <input
+            type='text'
+            id='name'
+            placeholder='your@email.com'
+            className='text-sm border rounded px-4 py-2 w-[225px]'
+          />
+        </div>
       </form>
     </div>
   );
