d1d9313c926dca7a442a971436f3a0411c7406af
eobcre <eobcre@gmail.com>
2023-08-26 20:42:36 -0400

    button styled

--- src/components/SignUpButton.tsx
+++ src/components/SignUpButton.tsx
@@ -3,8 +3,11 @@ import { Icon } from '@iconify/react';
 const SignUpButton: React.FC = () => {
   return (
     <div>
-      <button>
-        <Icon icon='flat-color-icons:google' />
+      <button className='flex items-center gap-8 bg-[#5B87E8] text-white font-arial rounded pl-1 pr-7 py-2'>
+        <Icon
+          icon='flat-color-icons:google'
+          className='bg-white rounded p-2 w-[38px] h-[38px]'
+        />
         Sign up with Google
       </button>
     </div>
