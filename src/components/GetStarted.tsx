eded0b69a3e13e3b48f47a3afa602141687442fc
eobcre <eobcre@gmail.com>
2023-08-26 22:09:32 -0400

    button styled

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -4,9 +4,11 @@ type ButtonProps = {
 
 const GetStarted: React.FC<ButtonProps> = ({ name }) => {
   return (
-    <>
-      <button>{name}</button>
-    </>
+    <div className='flex justify-center items-center mb-8 w-full'>
+      <button className='bg-[#E85C7D] text-white text-sm rounded py-2.5'>
+        {name}
+      </button>
+    </div>
   );
 };
 
